e8121f1ee75d07ff435ea1ecbc07445e9f027bf5
Shawn Lee <changoloco.evg@gmail.com>
2022-06-29 09:19:46 -0500

    :wrench: [chore]: configure windicss

---
+++ windi.config.ts
@@ -0,0 +1,19 @@
+export default {
+  theme: {
+    colors: {
+      blue: 'hsl(212, 86%, 64%)',
+      cyan: 'hsl(180, 62%, 55%)',
+      neutral: {
+        '50': 'hsl(0, 0%, 98%)',
+        '300': 'hsl(229, 6%, 66%)',
+        '700': 'hsl(234, 12%, 34%)',
+      },
+      orange: 'hsl(34, 97%, 64%)',
+      red: 'hsl(0, 78%, 62%)',
+      white: 'hsl(0, 0%, 100%)',
+    },
+    fontFamily: {
+      poppins: ['Poppins', 'sans-serif'],
+    },
+  },
+};
